a20ff68a62ac30e23c284118729670f813dd487b
Bill Lovett <bill@ilovett.com>
2019-10-16 15:24:01 -0400

    Get authorize command working
    The netrc file is no longer modified in favor of using environment
    variables exclusively.

--- src/authorize.nim
+++ src/authorize.nim
@@ -1,2 +1,62 @@
+import httpclient, json, terminal, uri
+
+type
+  AuthToken* = object
+    key*: string
+    value*: string
+
 proc authorize*() =
-  echo("Placeholder for setting up access credential.")
+  stdout.write("Notifier Server URL: ")
+
+  # var server = initUri()
+  # parseUri(stdin.readLine(), server)
+  # let server = parseUri(stdin.readLine())
+  var server = parseUri(stdin.readLine())
+
+  if server.hostname == "":
+    quit("Invalid URL, cannot continue.")
+
+  server.path = ""
+
+  stdout.write("Username: ")
+  let username = stdin.readLine()
+  if username == "":
+    quit("Username not specified, cannot continue.")
+
+  let password = readPasswordFromSTdin("Password: ")
+  if password == "":
+    quit("Password not specified, cannot continue.")
+
+  stdout.write("Label (optional): ")
+  let label = stdin.readLine()
+
+  let authUri = server / "auth"
+
+  let jsonPayload = $(%{
+    "username": %username,
+    "password": %password,
+    "label": %label
+  })
+
+  let client = newHttpClient()
+  client.headers = newHttpHeaders({
+    "Content-Type": "application/json",
+    "User-Agent": "notifier-cli"
+  })
+
+  let response = client.request(
+    $authUri,
+    httpMethod = HttpPost,
+    body = jsonPayload
+  )
+
+  if response.status != "200 OK":
+    quit("Failed to authentication with server.")
+
+
+  let authToken = to(parseJson(response.body), AuthToken)
+
+  echo("\c\l")
+  echo("NOTIFIER_URL=", $server)
+  echo("NOTIFIER_USER=", authToken.key)
+  echo("NOTIFIER_PASS=", authToken.value)
