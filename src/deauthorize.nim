49811d69349dbf3526c35ec5199edeae5a13d5de
Bill Lovett <bill@ilovett.com>
2019-10-19 14:24:44 -0400

    Add deauthorize command

---
+++ src/deauthorize.nim
@@ -0,0 +1,30 @@
+import base64, client, httpclient, json, strutils, uri
+
+proc deauthorize*(client: HttpClient, base: Uri) =
+
+  let endpoint = base / "deauth"
+
+  let encodedAuth = substr(
+    client.headers["Authorization"],
+    find(client.headers["Authorization"], ' ') + 1
+  )
+
+  let authFields = split(decode(encodedAuth), ':', 1)
+
+  let jsonPayload = %*{
+    "key": authFields[0],
+    "value": authFields[1]
+  }
+
+  let response = client.request(
+    $endpoint,
+    httpMethod = HttpPost,
+    body = $jsonPayload
+  )
+
+  if code(response) != Http200:
+    let responseBody = to(parseJson(response.body), NotifierError)
+    quit("Failed to deauthorize. " & responseBody.message)
+
+  echo("export NOTIFIER_USER=")
+  echo("export NOTIFIER_PASS=")
